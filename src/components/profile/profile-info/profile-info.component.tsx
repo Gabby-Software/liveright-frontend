a271b39851e06d2493eb6ef004fd3fee11b95735
yoseftuk <yoos@gmail.com>
2021-07-05 11:30:05 +0300

    profile for trainer

--- src/components/profile/profile-info/profile-info.component.tsx
+++ src/components/profile/profile-info/profile-info.component.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import Styles from './profile-info.styles';
 import {ProfileContext} from "../../../pages/profile/desktop-profile/profile.context";
 import FormTextarea from "../../forms/form-textarea/form-textarea.component";
+import {useTranslation} from "../../../modules/i18n/i18n.hook";
 
 type Prop = {
     formName:string;
@@ -10,6 +11,7 @@ type Prop = {
 }
 const ProfileInfo = ({name, value, formName}: Prop) => {
     const {editMode} = useContext(ProfileContext);
+    const {t} = useTranslation();
     return (
         <Styles>
             {
@@ -17,7 +19,7 @@ const ProfileInfo = ({name, value, formName}: Prop) => {
                     <FormTextarea name={formName} label={name}/>
                 ) : (
                     <>
-                        <div className={'info__name'}>{name}</div>
+                        <div className={'info__name'}>{name || t('no-data')}</div>
                         <div className={'info__value'}>{value}</div>
                     </>
                 )
