24d5535876e2990549d21b236e34a3255dea56e0
yoseftuk <yoos@gmail.com>
2021-07-09 13:48:08 +0300

    updates

--- src/hooks/authorization.hook.tsx
+++ src/hooks/authorization.hook.tsx
@@ -8,8 +8,41 @@ const messages = {
     CHECK_LOGIN: 'check_login',
     DO_LOGIN: 'do_login',
 };
-export const useAuthorization = (isAuthCallback: () => boolean, handleAuthCallback: (auth: any) => void) => {
-    document.domain = document.location.hostname;
+export type GenderType = 'male'|'female';
+export type AccountTypeType = 'client'|'trainer'|'org'|'admin';
+export type AccountType = {
+    id: number;
+    uuid: string;
+    type: AccountTypeType;
+    is_active: true;
+    last_used_at: string | null;
+    account_level: string | null;
+    is_current: boolean;
+    profile: null | string;
+
+}
+export type AuthObjectType = {
+    uuid: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    email_verified_at: string | null;
+    is_active: boolean;
+    birthday: string | null;
+    gender: GenderType;
+    avatar: string|null;
+    avatar_thumb: string | null;
+    created_at: string;
+    accounts: AccountType[]
+}
+
+export type AuthResponseType = {
+    access_token: string;
+    user: AuthObjectType;
+}
+
+export const useAuthorization = (isAuthCallback: () => AuthResponseType, handleAuthCallback: (auth: AuthResponseType) => void) => {
+
     useEffect(() => {
         const handler = ({data:{key, event, ...payload}}: MessageEvent<IframeEventType>) => {
             switch (event) {
@@ -17,7 +50,7 @@ export const useAuthorization = (isAuthCallback: () => boolean, handleAuthCallba
                     window.parent.postMessage({key, response: isAuthCallback()}, '*');
                     break;
                 case messages.DO_LOGIN:
-                    handleAuthCallback(payload);
+                    handleAuthCallback(payload as AuthResponseType);
                     window.parent.postMessage({key, response: 1}, '*');
             }
         };
--- src/pages/auth-handler/auth-handler.component.tsx
+++ src/pages/auth-handler/auth-handler.component.tsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import Styles from './auth-handler.styles';
-import {useAuthorization} from "../../hooks/authorization.hook";
+import {AuthObjectType, AuthResponseType, useAuthorization} from "../../hooks/authorization.hook";
 
 const AuthHandler = () => {
     useAuthorization(() => {
-        return !!localStorage.getItem('token')
+        const access_token = localStorage.getItem('token');
+        const user = JSON.parse(localStorage.getItem('auth') || '{}');
+        return {access_token, user} as AuthResponseType;
     }, (user) => {
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('auth', JSON.stringify(user.user));
         localStorage.setItem('token', user.access_token);
     });
     return null;
