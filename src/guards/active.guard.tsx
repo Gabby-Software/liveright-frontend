bc63a1ea65c33aabd22e061272cdf1f9d2ad51b5
yoseftuk <yoos@gmail.com>
2021-07-12 09:17:12 +0300

    fix active guard

--- src/guards/active.guard.tsx
+++ src/guards/active.guard.tsx
@@ -4,8 +4,8 @@ import {Routes} from "../enums/routes.enum";
 import {identity} from "../pipes/identity.pipe";
 
 export const onlyActive = (Component: React.ComponentType<any>) => (props: any) => {
-    const {is_active} = useAuth();
-    if(!is_active) {
+    const {email_verified_at} = useAuth();
+    if(!email_verified_at) {
         document.location.href = identity(Routes.REGISTER_CONFIRMATION);
         return null;
     }
