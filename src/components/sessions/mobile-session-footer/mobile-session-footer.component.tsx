103eced4c6cebcdbacb6e9c83e25214f03e33e4d
yoseftuk <yoos@gmail.com>
2021-07-13 10:38:32 +0300

    sessions - add session button

--- src/components/sessions/mobile-session-footer/mobile-session-footer.component.tsx
+++ src/components/sessions/mobile-session-footer/mobile-session-footer.component.tsx
@@ -1,9 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import Styles from './mobile-session-footer.styles';
+import {useTranslation} from "../../../modules/i18n/i18n.hook";
+import BottomButton from "../../bottom-button/bottom-button.component";
+import AddInvoiceModal from "../../invoices/add-invoice-modal/add-invoice-modal.component";
 
-type Props = {};
-const MobileSessionFooter = ({}:Props) => {
-    return null;
+const MobileSessionFooter = () => {
+    const {t} = useTranslation();
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    return (
+        <>
+            <BottomButton type={'primary'} className={'sessions-footer__add'}
+                          onClick={() => setIsFormOpen(true)}
+            >{t('sessions:add')}</BottomButton>
+            {/*<AddInvoiceModal isOpen={isFormOpen} onClose={() => setIsFormOpen(false)}/>*/}
+        </>
+    );
 };
 
 export default MobileSessionFooter;
--- src/enums/session-filters.enum.ts
+++ src/enums/session-filters.enum.ts
@@ -4,6 +4,7 @@ export const sessionStatusOptions:OptionType[] = [
     {label: 'All', value: 'All'},
     {label: 'Booked', value: 'Booked'},
     {label: 'Cancelled', value: 'Cancelled'},
+    {label: 'Completed', value: 'Completed'},
 ];
 
 export const sessionTimelineOptions:OptionType[] = [
@@ -15,5 +16,6 @@ export const sessionTimelineOptions:OptionType[] = [
 export const sessionTypeOptions:OptionType[] = [
     {label: 'All', value: 'All'},
     {label: 'PT Session', value: 'PT Session'},
-    {label: 'Consultation', value: 'Consultation'}
+    {label: 'Consultation', value: 'Consultation'},
+    {label: 'Other', value: 'Other'}
 ];
