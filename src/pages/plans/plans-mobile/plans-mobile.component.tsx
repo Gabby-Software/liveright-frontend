491e983d5d7b066c76a5ad37756e7969d1bf2478
yoseftuk <yoos@gmail.com>
2021-07-13 22:53:11 +0300

    training split view - mobile

--- src/pages/plans/plans-mobile/plans-mobile.component.tsx
+++ src/pages/plans/plans-mobile/plans-mobile.component.tsx
@@ -4,14 +4,25 @@ import {FormSwitchUI} from "../../../components/forms/form-switch/form-switch.co
 import {OptionType} from "../../../types/option.type";
 import {useTranslation} from "../../../modules/i18n/i18n.hook";
 import {planTypes} from "../../../enums/plan-types.enum";
+import TrainingSplitsMobile from "./training-splits-mobile/training-splits-mobile.component";
+import MealPlansMobile from "./meal-plans-mobile/meal-plans-mobile.component";
+import WorkoutPlansMobile from "./workout-plans-mobile/workout-plans-mobile.component";
 
 type Props = {
     plan: string;
 };
-const PlansMobile = ({}:Props) => {
-    const {t} = useTranslation();
+const PlansMobile = ({plan}:Props) => {
     return (
         <Styles>
+            {
+                plan === planTypes.TRAINING_SPLIT?(
+                    <TrainingSplitsMobile/>
+                ) : plan === planTypes.DIET_PLAN?(
+                    <MealPlansMobile/>
+                ) : plan === planTypes.WORKOUT_PLAN?(
+                    <WorkoutPlansMobile/>
+                ):null
+            }
         </Styles>
     )
 };
--- src/pages/plans/plans-mobile/training-splits-mobile/training-splits-mobile.component.tsx
+++ src/pages/plans/plans-mobile/training-splits-mobile/training-splits-mobile.component.tsx
@@ -1,9 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import Styles from './training-splits-mobile.styles';
+import {useAuth} from "../../../../hooks/auth.hook";
+import {useTranslation} from "../../../../modules/i18n/i18n.hook";
+import {useInfiniteScroll} from "../../../../hooks/infinite-scroll.hook";
+import {trainingSplitData} from "../../plans.data";
+import Card from "../../../../components/card/card.style";
+import {date} from "../../../../pipes/date.pipe";
+import {capitalize} from "../../../../pipes/capitalize.pipe";
 
-type Props = {};
-const TrainingSplitsMobile = ({}:Props) => {
-    return null;
+const TrainingSplitsMobile = () => {
+    const [page, setPage] = useState(1);
+    const {type} = useAuth();
+    const {t} = useTranslation();
+    useInfiniteScroll((p: number) => {
+        setPage(p);
+        return Promise.resolve(p - 1 > trainingSplitData.length / 10)
+    });
+    return (
+        <Styles>
+            {
+                trainingSplitData.slice(0, page*10).map(({split_name, start_date, status}) => (
+                    <Card className={'split__card'}>
+                        <div className={'split__left'}>
+                            <div className={'split__name'}>{split_name}</div>
+                            <div className={'split__date'}>{t('plans:started')} {date(start_date)}</div>
+                        </div>
+                        <div className={`split__status split__status__${status}`}>{capitalize(status)}</div>
+                    </Card>
+                ))
+            }
+        </Styles>
+    )
 };
 
 export default TrainingSplitsMobile;
