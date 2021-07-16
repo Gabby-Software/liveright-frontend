98fbe5b73373bb28c0469974c4f383df616d908c
yoseftuk <yoos@gmail.com>
2021-07-16 17:41:34 +0300

    update trainer/client guards

--- src/guards/trainer.guard.tsx
+++ src/guards/trainer.guard.tsx
@@ -2,10 +2,12 @@ import React, {ComponentProps} from "react";
 import {useAuth} from "../hooks/auth.hook";
 import userTypes from "../enums/user-types.enum";
 import PageNotFound from "../pages/page-not-found/page-not-found.component";
+import {Redirect} from "react-router";
+import {Routes} from "../enums/routes.enum";
 
 export const onlyTrainer = (Component: React.ComponentType<any>) => (props: ComponentProps<any>) => {
     const {type} = useAuth();
     if(type !== userTypes.TRAINER)
-        return <PageNotFound/>;
+        return <Redirect to={Routes.HOME}/>;
     return <Component {...props}/>;
 };
