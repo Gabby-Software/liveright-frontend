6c7d12606c0c78ef5e7c0592a4048a8b27f0e506
yoseftuk <yoos@gmail.com>
2021-07-19 18:21:59 +0300

    sidebar+footer changes - part 3

--- src/layouts/desktop-footer/desktop-footer.component.tsx
+++ src/layouts/desktop-footer/desktop-footer.component.tsx
@@ -10,13 +10,32 @@ import {Link} from "react-router-dom";
 import {identity} from "../../pipes/identity.pipe";
 import {Routes} from "../../enums/routes.enum";
 import {classes} from "../../pipes/classes.pipe";
+import {useDispatch} from "react-redux";
+import {ACTION_LOGOUT_REQUEST, ACTION_SWITCH_ACCOUNT_REQUEST} from "../../store/action-types";
+import {toast} from "../../components/toast/toast.component";
 
 
 type Props = {};
 const DesktopFooter = ({}: Props) => {
-    const {first_name, last_name, avatar, type} = useAuth();
+    const {first_name, last_name, avatar, type, accounts} = useAuth();
     const [isOpen, setIsOpen] = useState(false);
+    const dispatch = useDispatch();
     const {t} = useTranslation();
+    const logout = () => {
+        dispatch({type: ACTION_LOGOUT_REQUEST});
+    };
+    const switchAccount = () => {
+        const uuid = accounts.find(acc => !acc.is_current)?.uuid;
+        dispatch({
+            type: ACTION_SWITCH_ACCOUNT_REQUEST,
+            payload: {
+                uuid,
+                onSuccess: () => {
+                    toast.show({type: 'success', msg: t('alerts:switch-account-success')});
+                }
+            }
+        });
+    };
     return (
         <Styles className={classes('footer', isOpen&&'footer__open')}>
             <div className={'footer__wrapper'}>
@@ -31,7 +50,13 @@ const DesktopFooter = ({}: Props) => {
                 <div className={'footer__actions'}>
                     <div className={'footer__actions__title'}>{t('what-to-do')}</div>
                     <div className={'footer__actions__cont'}>
-                        <FormButton type={'ghost'} className={'footer__action'}>Switch to client</FormButton>
+                        {
+                            accounts.length > 1?  (
+                                <FormButton type={'ghost'} className={'footer__action'} onClick={switchAccount}>{
+                                    t('switch-to', {type: t(accounts.find(acc => !acc.is_current)?.type||'')})
+                                }</FormButton>
+                            ):null
+                        }
                         <a href={identity(Routes.PROFILE)} className={'footer__action'}>
                             <FormButton type={'ghost'}>Edit my information</FormButton>
                         </a>
@@ -44,7 +69,8 @@ const DesktopFooter = ({}: Props) => {
                         <Link to={'#'} className={'footer__action'}>
                             <FormButton type={'ghost'}>Get Help</FormButton>
                         </Link>
-                        <FormButton type={'primary'} className={'footer__action'}>Log Out</FormButton>
+                        <FormButton type={'primary'} className={'footer__action'}
+                            onClick={logout}>Log Out</FormButton>
                     </div>
                 </div>
             </div>
