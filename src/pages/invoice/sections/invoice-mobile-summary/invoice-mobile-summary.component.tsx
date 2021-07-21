078dbe5e69faa6f2ada388ee025182d396e4b6b2
yoseftuk <yoos@gmail.com>
2021-07-21 11:56:12 +0300

    invoice details - mobile - 2

--- src/pages/invoice/sections/invoice-mobile-summary/invoice-mobile-summary.component.tsx
+++ src/pages/invoice/sections/invoice-mobile-summary/invoice-mobile-summary.component.tsx
@@ -1,9 +1,55 @@
 import React, {useState, useEffect} from 'react';
 import Styles from './invoice-mobile-summary.styles';
 
-type Props = {};
-const InvoiceMobileSummary = ({}:Props) => {
-    return null;
+const InvoiceMobileSummary = () => {
+    return (
+        <Styles className={'invoice-m-summary'}>
+            <div className={'invoice-m-summary__row'}>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>Item</div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>Cost</div>
+            </div>
+            <div className={'invoice-m-summary__row'}>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>
+                    <div className={'invoice-m-summary__item invoice-m-summary__ebold'}>Coaching</div>
+                    <div className={'invoice-m-summary__item'}>Consultation for one</div>
+                </div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>461 USD</div>
+            </div>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__space'}>
+                <div className={'invoice-m-summary__item'}>1 X 461 USD</div>
+                <div className={'invoice-m-summary__item'}>(46.1 USD VAT)</div>
+            </div>
+            <div className={'invoice-m-summary__row'}>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>
+                    <div className={'invoice-m-summary__item invoice-m-summary__ebold'}>Session</div>
+                    <div className={'invoice-m-summary__item'}>PT Session</div>
+                </div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>461 USD</div>
+            </div>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__space'}>
+                <div className={'invoice-m-summary__item'}>1 X 461 USD</div>
+                <div className={'invoice-m-summary__item'}>(46.1 USD VAT)</div>
+            </div>
+            <div className={'invoice-m-summary__hr'}/>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__big'}>
+                <div className={'invoice-m-summary__item'}>Subtotal</div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>922 USD</div>
+            </div>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__big'}>
+                <div className={'invoice-m-summary__item'}>VAT (10%)</div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>92.2 USD</div>
+            </div>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__big'}>
+                <div className={'invoice-m-summary__item'}>Discounts</div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>0 USD</div>
+            </div>
+            <div className={'invoice-m-summary__hr'}/>
+            <div className={'invoice-m-summary__row invoice-m-summary__row__big'}>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>Total Payable</div>
+                <div className={'invoice-m-summary__item invoice-m-summary__bold'}>922 USD</div>
+            </div>
+        </Styles>
+    );
 };
 
 export default InvoiceMobileSummary;
--- src/pages/invoice/sections/invoice-mobile-summary/invoice-mobile-summary.styles.ts
+++ src/pages/invoice/sections/invoice-mobile-summary/invoice-mobile-summary.styles.ts
@@ -1,3 +1,35 @@
 import styled from "styled-components";
 
-export default styled.div``;
+export default styled.div`
+font-size: 16px;
+font-weight: 400;
+.invoice-m-summary {
+
+    &__row {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 13px;
+        font-size: 16px;
+        &__space {
+            margin-bottom: 36px;
+        }
+        &__big {
+            font-size: 18px;
+        }
+    }
+    &__item {
+        white-space: nowrap;
+    }
+    &__bold {
+        font-weight: 500;
+    }
+    &__ebold {
+        font-weight: 600;
+    }
+    &__hr {
+        margin: 32px -20px;
+        width: calc(100% + 40px);
+        border-top: 1px solid ${p => p.theme.vars.colors.light};
+    }
+}
+`;
