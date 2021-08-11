6fe4a68f7c95ac2a44b23af74cd74a77001680ad
yoseftuk <yoostuk@gmail.com>
2021-08-11 11:51:06 +0300

    [clients][mobile] auto refetch clients on add new client

--- src/components/clients/mobile-client-footer/mobile-client-footer.component.tsx
+++ src/components/clients/mobile-client-footer/mobile-client-footer.component.tsx
@@ -3,16 +3,29 @@ import Styles from './mobile-client-footer.styles';
 import BottomButton from "../../bottom-button/bottom-button.component";
 import {useTranslation} from "../../../modules/i18n/i18n.hook";
 import AddClientModal from "../add-client-modal/add-client-modal.component";
+import {useClients} from "../../../hooks/clients.hook";
+import {useDispatch} from "react-redux";
+import {ACTION_GET_CLIENTS_REQUEST} from "../../../store/action-types";
 
 const MobileClientFooter = () => {
     const [isOpen, setIsOpen] = useState(false);
     const {t} = useTranslation();
+    const {filters} = useClients();
+    const dispatch = useDispatch();
+    const refetchClients = () => {
+        dispatch({
+            type: ACTION_GET_CLIENTS_REQUEST, payload: {
+                ...filters,
+                page: 1
+            }
+        })
+    }
     return (
         <>
             <BottomButton type={'primary'}
                           onClick={() => setIsOpen(true)}
             >{t('clients:add')}</BottomButton>
-            <AddClientModal isOpen={isOpen} onClose={() => setIsOpen(false)}/>
+            <AddClientModal isOpen={isOpen} onClose={() => setIsOpen(false)} onSubmit={refetchClients}/>
         </>
     );
 };
