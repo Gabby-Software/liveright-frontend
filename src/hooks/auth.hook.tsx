cf09d7e6cccf5469392aaeca1d768dd65b0e68f5
yoseftuk <yoostuk@gmail.com>
2021-08-13 16:05:04 +0300

    switch to account based notifications

--- src/hooks/auth.hook.tsx
+++ src/hooks/auth.hook.tsx
@@ -5,7 +5,7 @@ import {AccountObjType, AccountType} from "../types/account.type";
 
 export const useAuth = () => {
     return useSelector((state: RootState) => ({
-        ...(state.auth as AccountObjType)?.accounts?.find(a => a.is_current),
         ...state.auth as AccountObjType,
+        ...(state.auth as AccountObjType)?.accounts?.find(a => a.is_current),
     } as (AccountObjType & AccountType)));
 };
