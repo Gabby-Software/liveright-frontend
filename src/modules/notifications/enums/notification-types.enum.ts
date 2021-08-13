bd8af4c5d85e109019a8cc0a0d379dc00d5e8cce
yoseftuk <yoostuk@gmail.com>
2021-08-13 11:30:49 +0300

    notifications FE - part 3

--- src/modules/notifications/enums/notification-types.enum.ts
+++ src/modules/notifications/enums/notification-types.enum.ts
@@ -1,10 +1,7 @@
 export const notificationsTypes = {
-    // INVITATION_ACCEPT: 'invitation_accepted_notification',
-    // INVITATION_REJECT: 'invitation_rejected_notification',
-    // INVITATION_RECEIVED: 'training_invitation',
-    INVITATION_ACCEPT: 'App\\Notifications\\Invitation\\TrainingInvitation',
-    INVITATION_REJECT: 'App\\Notifications\\Invitation\\TrainingInvitation',
-    INVITATION_RECEIVED: 'App\\Notifications\\Invitation\\TrainingInvitation',
+    INVITATION_ACCEPT: 'invitation_accepted_notification',
+    INVITATION_REJECT: 'invitation_rejected_notification',
+    INVITATION_RECEIVED: 'training_invitation',
     INVOICE_STATUS_CHANGED: 'invoice_status_changed_notification',
     INVOICE_CREATED: 'invoice_created_notification',
     SESSION_REQUESTED: 'client_requested_session',
