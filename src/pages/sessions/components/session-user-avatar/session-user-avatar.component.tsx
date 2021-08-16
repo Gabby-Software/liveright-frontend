d3b6c60da784c1b97e1ebb1486e88edd2ead2fce
Qweest <daniilgera1@gmail.com>
2021-08-16 12:52:43 +0300

    Fix fallback avatar

--- src/pages/sessions/components/session-user-avatar/session-user-avatar.component.tsx
+++ src/pages/sessions/components/session-user-avatar/session-user-avatar.component.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Styles, {ProfileImageStyled} from './session-user-avatar.styles';
 import {noImage} from "../../../../pipes/no-image.pipe";
+import api from "../../../../managers/api.manager";
 
 interface Props {
     avatar?: {url: string} | null;
@@ -10,10 +11,26 @@ interface Props {
 
 const SessionUserAvatar: React.FC<Props> = (props) => {
     const {avatar, first_name = '', last_name = ''} = props;
+    const [url, setUrl] = useState('');
+
+    useEffect(() => {
+        if (avatar?.url) {
+            const checkUrl = async () => {
+                try {
+                    await api.get(avatar.url);
+                    setUrl(avatar.url);
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+
+            checkUrl();
+        }
+    }, [])
 
     return (
         <Styles>
-          <ProfileImageStyled url={avatar?.url} placeholder={noImage(first_name, last_name)} />
+          <ProfileImageStyled url={url} placeholder={noImage(first_name, last_name)} />
           {first_name} {last_name}
         </Styles>
     );
