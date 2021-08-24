f78889029863301ca18c75d670256bb81586b1ea
yoseftuk <yoostuk@gmail.com>
2021-08-24 10:33:12 +0300

    [chat][files] update file type with name+mime+size

--- src/pages/chat/chat.data.ts
+++ src/pages/chat/chat.data.ts
@@ -48,169 +48,169 @@ export const mockRooms: ChatRoomType[] = [
 ]
 
 export const mockMessages: ChatMessageType[] = [
-  {
-    meta: {
-      sent_at: moment().add(-2, 'days').format(),
-      delivered_at: moment().add(-2, 'days').format(),
-      read_at: moment().add(-2, 'days').format()
-    },
-    content: {
-      text: 'Hello World',
-      files: [],
-      embedLinks: []
-    },
-    types: ['text'],
-    _id: '611d974911dcb53c1064fd71',
-    senderId: '123',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-1, 'days').format(),
-      delivered_at: moment().add(-1, 'days').format(),
-      read_at: moment().add(-1, 'days').format()
-    },
-    content: {
-      text: '',
-      files: ['https://picsum.photos/id/1/1200/920'],
-      embedLinks: []
-    },
-    types: ['image'],
-    _id: '611d974911dcb53c1064fd72',
-    senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-1, 'hour').format(),
-      delivered_at: moment().add(-1, 'hour').format(),
-      read_at: moment().add(-1, 'hour').format()
-    },
-    content: {
-      text: 'Lorem ipsum dolor sit ammet',
-      files: [
-        'https://picsum.photos/id/1/1200/920',
-        'https://picsum.photos/id/8/1200/920'
-      ],
-      embedLinks: []
-    },
-    types: ['text', 'image', 'image'],
-    _id: '611d974911dcb53c1064fd73',
-    senderId: '123',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-23, 'minutes').format(),
-      delivered_at: moment().add(-23, 'minutes').format(),
-      read_at: null
-    },
-    content: {
-      text: '',
-      files: ['http://www.africau.edu/images/default/sample.pdf'],
-      embedLinks: []
-    },
-    types: ['file'],
-    _id: '611d974911dcb53c1064fd74',
-    senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-23, 'minutes').format(),
-      delivered_at: moment().add(-23, 'minutes').format(),
-      read_at: null
-    },
-    content: {
-      text: '',
-      files: [
-        'https://picsum.photos/id/53/1080/920',
-        'https://picsum.photos/id/67/800/800',
-        'https://picsum.photos/id/46/800/600',
-        'https://picsum.photos/id/23/1080/1200',
-        'https://picsum.photos/id/54/1200/1080',
-        'https://picsum.photos/id/55/920/1200',
-        'https://picsum.photos/id/56/600/920',
-        'https://picsum.photos/id/77/920/800'
-      ],
-      embedLinks: []
-    },
-    types: [
-      'image',
-      'image',
-      'image',
-      'image',
-      'image',
-      'image',
-      'image',
-      'image'
-    ],
-    _id: '611d974911dcb53c1064fd75',
-    senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-3, 'minutes').format(),
-      delivered_at: moment().add(-3, 'minutes').format(),
-      read_at: null
-    },
-    content: {
-      text: '',
-      files: [
-        'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
-      ],
-      embedLinks: []
-    },
-    types: ['audio'],
-    _id: '611d974911dcb63c1064fd75',
-    senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  },
-  {
-    meta: {
-      sent_at: moment().add(-3, 'minutes').format(),
-      delivered_at: moment().add(-3, 'minutes').format(),
-      read_at: null
-    },
-    content: {
-      text: '',
-      files: [
-        'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
-      ],
-      embedLinks: []
-    },
-    types: ['audio'],
-    _id: '611d974911dcb63c1064id75',
-    senderId: '123',
-    receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
-    chat_room_id: '6116a782093e0a5e500e8eb6',
-    createdAt: '2021-08-18T23:27:05.383Z',
-    updatedAt: '2021-08-18T23:27:05.383Z',
-    __v: 0
-  }
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-2, 'days').format(),
+  //     delivered_at: moment().add(-2, 'days').format(),
+  //     read_at: moment().add(-2, 'days').format()
+  //   },
+  //   content: {
+  //     text: 'Hello World',
+  //     files: [],
+  //     embedLinks: []
+  //   },
+  //   types: ['text'],
+  //   _id: '611d974911dcb53c1064fd71',
+  //   senderId: '123',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-1, 'days').format(),
+  //     delivered_at: moment().add(-1, 'days').format(),
+  //     read_at: moment().add(-1, 'days').format()
+  //   },
+  //   content: {
+  //     text: '',
+  //     files: ['https://picsum.photos/id/1/1200/920'],
+  //     embedLinks: []
+  //   },
+  //   types: ['image'],
+  //   _id: '611d974911dcb53c1064fd72',
+  //   senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-1, 'hour').format(),
+  //     delivered_at: moment().add(-1, 'hour').format(),
+  //     read_at: moment().add(-1, 'hour').format()
+  //   },
+  //   content: {
+  //     text: 'Lorem ipsum dolor sit ammet',
+  //     files: [
+  //       'https://picsum.photos/id/1/1200/920',
+  //       'https://picsum.photos/id/8/1200/920'
+  //     ],
+  //     embedLinks: []
+  //   },
+  //   types: ['text', 'image', 'image'],
+  //   _id: '611d974911dcb53c1064fd73',
+  //   senderId: '123',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-23, 'minutes').format(),
+  //     delivered_at: moment().add(-23, 'minutes').format(),
+  //     read_at: null
+  //   },
+  //   content: {
+  //     text: '',
+  //     files: ['http://www.africau.edu/images/default/sample.pdf'],
+  //     embedLinks: []
+  //   },
+  //   types: ['file'],
+  //   _id: '611d974911dcb53c1064fd74',
+  //   senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-23, 'minutes').format(),
+  //     delivered_at: moment().add(-23, 'minutes').format(),
+  //     read_at: null
+  //   },
+  //   content: {
+  //     text: '',
+  //     files: [
+  //       'https://picsum.photos/id/53/1080/920',
+  //       'https://picsum.photos/id/67/800/800',
+  //       'https://picsum.photos/id/46/800/600',
+  //       'https://picsum.photos/id/23/1080/1200',
+  //       'https://picsum.photos/id/54/1200/1080',
+  //       'https://picsum.photos/id/55/920/1200',
+  //       'https://picsum.photos/id/56/600/920',
+  //       'https://picsum.photos/id/77/920/800'
+  //     ],
+  //     embedLinks: []
+  //   },
+  //   types: [
+  //     'image',
+  //     'image',
+  //     'image',
+  //     'image',
+  //     'image',
+  //     'image',
+  //     'image',
+  //     'image'
+  //   ],
+  //   _id: '611d974911dcb53c1064fd75',
+  //   senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-3, 'minutes').format(),
+  //     delivered_at: moment().add(-3, 'minutes').format(),
+  //     read_at: null
+  //   },
+  //   content: {
+  //     text: '',
+  //     files: [
+  //       'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
+  //     ],
+  //     embedLinks: []
+  //   },
+  //   types: ['audio'],
+  //   _id: '611d974911dcb63c1064fd75',
+  //   senderId: '941cbce2-fa1f-4e0f-9afe-2b04369a819a',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // },
+  // {
+  //   meta: {
+  //     sent_at: moment().add(-3, 'minutes').format(),
+  //     delivered_at: moment().add(-3, 'minutes').format(),
+  //     read_at: null
+  //   },
+  //   content: {
+  //     text: '',
+  //     files: [
+  //       'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
+  //     ],
+  //     embedLinks: []
+  //   },
+  //   types: ['audio'],
+  //   _id: '611d974911dcb63c1064id75',
+  //   senderId: '123',
+  //   receiverId: '941cbd93-10c1-4a0d-b922-b39908a7227d',
+  //   chat_room_id: '6116a782093e0a5e500e8eb6',
+  //   createdAt: '2021-08-18T23:27:05.383Z',
+  //   updatedAt: '2021-08-18T23:27:05.383Z',
+  //   __v: 0
+  // }
 ]
--- src/pages/chat/components/chat-message-attachment/chat-message-attachment.component.tsx
+++ src/pages/chat/components/chat-message-attachment/chat-message-attachment.component.tsx
@@ -1,29 +1,34 @@
 import React, { FC } from 'react'
 
 import { chatIcons } from '../../../../modules/chat/enums/chat-icons.enum'
+import { filesize } from '../../../../modules/chat/pipes/filesize.pipe'
+import { ChatFileType } from '../../../../modules/chat/types/chat-file.type'
+import { KnownFileTypesType } from '../../../../modules/chat/types/known-file-types.type'
 import { classes } from '../../../../pipes/classes.pipe'
+import { excerpt } from '../../../../pipes/excerpt.pipe'
 import Styles from './chat-message-attachment.styles'
 
 type Props = {
-  file: string
+  file: ChatFileType
   me?: boolean
 }
 const ChatMessageAttachment: FC<Props> = ({ file, me }) => {
-  const Icon = chatIcons['pdf']
+  const type = file.original_name.split('.').pop() as KnownFileTypesType
+  const Icon = chatIcons[type]
   return (
     <Styles
       className={classes('cm-file', me && 'me')}
       target={'_blank'}
-      download={'Example File.pdf'}
-      href={file}
+      download={file.original_name}
+      href={file.url}
     >
       <Icon className={'cm-file__icon'} />
       <div>
-        <div className={'cm-file__name'}>Example File.pdf</div>
+        <div className={'cm-file__name'}>{excerpt(file.original_name, 30)}</div>
         <div className={'cm-file__data'}>
-          <span>PDF</span>
+          <span>{type.toUpperCase()}</span>
           <span className={'cm-file__divider'} />
-          <span>45 KB</span>
+          <span>{filesize(file.size)}</span>
         </div>
       </div>
     </Styles>
