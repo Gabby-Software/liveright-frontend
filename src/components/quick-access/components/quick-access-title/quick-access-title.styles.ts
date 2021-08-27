f9c60e44ecb1c149eb12fca2c679562dacbbfec4
yoseftuk <yoostuk@gmail.com>
2021-08-27 12:30:18 +0300

    [quick log][client] part 2

---
+++ src/components/quick-access/components/quick-access-title/quick-access-title.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  background-color: ${(p) => p.theme.vars.colors.primaryDark_v2};
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 20px;
+`
