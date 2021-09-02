a6cc0acbb13b524fae81ddba5da83f6af388f8d6
yoseftuk <yoostuk@gmail.com>
2021-09-02 21:24:58 +0300

    chat notification label - mobile+desktop

--- src/components/chat-icon/chat-icon.styles.ts
+++ src/components/chat-icon/chat-icon.styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
+import { media } from '../../assets/styles/_media'
+
 const wShrink = '10px'
 const wGrow = '16px'
 export default styled.div`
+  ${(p) => p.theme.extend.flexCenter}
   &:after {
     position: absolute;
     top: -5px;
@@ -18,6 +21,12 @@ export default styled.div`
     cursor: pointer;
     transition: ${(p) => p.theme.vars.defaults.transition};
     background-color: ${(p) => p.theme.vars.colors.primary};
+    ${media('tablet', 'min')`
+        top:0;
+        bottom:0;
+        right:0;
+        margin:auto;
+    `}
   }
   &.notification {
     &__active {
