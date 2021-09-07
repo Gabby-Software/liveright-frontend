6247d8d95bdaa076c484ae8d4d4d4ecd2e58b857
yoseftuk <yoostuk@gmail.com>
2021-09-07 14:45:46 +0300

    [health data] switch date of highlights

--- src/pages/progress/components/progress-health-data/progress-health-data.styles.ts
+++ src/pages/progress/components/progress-health-data/progress-health-data.styles.ts
@@ -6,8 +6,35 @@ export const Wrapper = styled.div`
   .select_input__wrapper {
     width: 250px;
   }
+  .today-highlights {
+    display: flex;
+    align-items: center;
+  }
 `
-
+export const HighlightArrow = styled.span<{ disabled: boolean }>`
+  transition: ${(p) => p.theme.vars.defaults.transition};
+  cursor: pointer;
+  ${({ disabled }) =>
+    disabled
+      ? `
+    opacity: 0;
+    pointer-events: none;
+    touch-action: none;
+  `
+      : ''};
+  &:first-child {
+    margin-right: 6px;
+  }
+  &:last-child {
+    margin-left: 6px;
+    transform: scaleX(-1);
+  }
+  svg {
+    display: block;
+    width: 24px;
+    height: 24px;
+  }
+`
 export const CardsWrapper = styled.div`
   margin-bottom: 32px;
   padding: 8px 0;
