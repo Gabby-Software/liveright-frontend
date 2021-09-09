5b45b31d5b71826b7af3d73eace7fc1ae4d42956
Andriy Halushka <andrushagalushka@gmail.com>
2021-09-10 02:22:32 +0300

    merge, resolve conflicts

--- src/pages/invoice/components/icon-actions/icon-actions.component.tsx
+++ src/pages/invoice/components/icon-actions/icon-actions.component.tsx
@@ -62,7 +62,8 @@ export default function IconActions({
               invoice_id: '' + data.id,
               total: data.total,
               currency: data.currency.code,
-              status: data.status
+              status: data.status,
+              name: `${data.invoice_to.user.first_name} ${data.invoice_to.user.last_name}`
             })
           }}
         >
