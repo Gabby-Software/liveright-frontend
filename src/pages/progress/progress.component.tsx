82ca64117323c47a689f52538284d147f1956817
Andriy Halushka <andrushagalushka@gmail.com>
2021-09-21 01:41:25 +0300

    refactor

--- src/pages/progress/progress.component.tsx
+++ src/pages/progress/progress.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { useIsMobile } from '../../hooks/is-mobile.hook'
-// import { PROGRESS_LOG_URL } from './progress.constants'
 import ProgressDesktop from './progress-desktop/progress-desktop.component'
 import ProgressMobile from './progress-mobile/progress-mobile.component'
 
