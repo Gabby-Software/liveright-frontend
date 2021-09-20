2437c6153312161eb9eecba5f7f6014a65d8c88f
Andriy Halushka <andrushagalushka@gmail.com>
2021-09-20 23:58:20 +0300

    health add chart

--- src/pages/progress/components/progress-chart/progress-chart.component.tsx
+++ src/pages/progress/components/progress-chart/progress-chart.component.tsx
@@ -1,7 +1,139 @@
-import React from 'react'
+import moment from 'moment'
+import { useContext } from 'react'
+import { Tooltip } from 'recharts'
 
-const HealthChart = () => {
-  return <div>TBD</div>
+import { colors } from '../../../../assets/styles/_variables'
+import LineChart from '../../../../components/charts/line-chart/line-chart.component'
+import { TooltipContainer } from '../../../../components/charts/tooltip'
+import {
+  EmptyPlaceholder,
+  LoadingPlaceholder
+} from '../../../../components/placeholders'
+import { HealthOnlyInclude } from '../../../../hooks/api/progress/useHealth'
+import { useTranslation } from '../../../../modules/i18n/i18n.hook'
+import { capitalize } from '../../../../pipes/capitalize.pipe'
+import { DATE_FORMAT, DATE_MONTH_RENDER_FORMAT } from '../../../../utils/date'
+import { shortNum } from '../../../../utils/numbers'
+import { HealthData, QualityType } from '../../progress.types'
+import ProgressHealthDataContext from '../progress-health-data/progress-health-data.context'
+import { Styles } from './progress-chart.styles'
+
+export default function HealthChart() {
+  const { t } = useTranslation()
+  const { health, onlyInclude, isLoading } = useContext(
+    ProgressHealthDataContext
+  )
+
+  const chartData = formatChartData(health, onlyInclude)
+
+  return (
+    <Styles>
+      <div className="chart__title-container">
+        <h4 className="chart__title">{t(`progress:${onlyInclude}`)}</h4>
+      </div>
+
+      {isLoading ? (
+        <LoadingPlaceholder spacing />
+      ) : !health.length ? (
+        <EmptyPlaceholder spacing />
+      ) : (
+        <div className="chart__chart-container">
+          <LineChart
+            height={250}
+            data={chartData}
+            xDataKey="date"
+            dataKeys={['value']}
+            dataStroke={[colors.secondary4_v2]}
+            yTickFormatter={
+              onlyInclude === 'steps'
+                ? (tick: number) => shortNum(tick)
+                : undefined
+            }
+            tooltip={<Tooltip cursor={false} content={<TooltipContent />} />}
+          />
+        </div>
+      )}
+    </Styles>
+  )
 }
 
-export default HealthChart
+function formatChartData(health: HealthData[], onlyInclude: HealthOnlyInclude) {
+  try {
+    if (onlyInclude === 'sleep') {
+      return health.map((h) => ({
+        value: moment.duration(h.sleep?.sleep_duration).asHours(),
+        date: moment(h.date, DATE_FORMAT).format(DATE_MONTH_RENDER_FORMAT),
+        dotColor: getDotColor(h.sleep?.quality),
+        label: 'Sleep',
+        tooltipValue: moment.duration(h.sleep?.sleep_duration).asHours() + 'h',
+        quality: h.sleep?.quality ? capitalize(h.sleep?.quality) : '-'
+      }))
+    }
+    if (onlyInclude === 'heart_rate') {
+      return health.map((h) => ({
+        value: h.heart_rate?.avg_rate ? Number(h.heart_rate.avg_rate) : 0,
+        date: moment(h.date, DATE_FORMAT).format(DATE_MONTH_RENDER_FORMAT),
+        dotColor: getDotColor(h.heart_rate?.quality),
+        label: 'Heart rate',
+        tooltipValue: h.heart_rate?.avg_rate
+          ? Number(h.heart_rate.avg_rate)
+          : 0,
+        quality: h.heart_rate?.quality ? capitalize(h.heart_rate?.quality) : '-'
+      }))
+    }
+    if (onlyInclude === 'steps') {
+      return health.map((h) => ({
+        value: h.steps?.daily_steps ? Number(h.steps.daily_steps) : 0,
+        date: moment(h.date, DATE_FORMAT).format(DATE_MONTH_RENDER_FORMAT),
+        dotColor: getDotColor(h.steps?.quality),
+        label: 'Steps',
+        tooltipValue: h.steps?.daily_steps
+          ? shortNum(Number(h.steps.daily_steps))
+          : 0,
+        quality: h.steps?.quality ? capitalize(h.steps?.quality) : '-'
+      }))
+    }
+    if (onlyInclude === 'blood_glucose') {
+      return health.map((h) => ({
+        value: h.blood_glucose?.glucose ? Number(h.blood_glucose.glucose) : 0,
+        date: moment(h.date, DATE_FORMAT).format(DATE_MONTH_RENDER_FORMAT),
+        dotColor: getDotColor(h.blood_glucose?.quality),
+        label: 'Blood Glucose',
+        tooltipValue: h.blood_glucose?.glucose
+          ? Number(h.blood_glucose.glucose) + 'Mg/dl'
+          : 0,
+        quality: h.blood_glucose?.quality
+          ? capitalize(h.blood_glucose?.quality)
+          : '-'
+      }))
+    }
+    return []
+  } catch (e) {
+    console.error(e)
+    return []
+  }
+}
+
+function getDotColor(quality?: QualityType): string {
+  switch (quality) {
+    case 'low':
+      return '#EF1733'
+    case 'high':
+    case 'good':
+      return '#00B334'
+    default:
+      return '#5E5E5E'
+  }
+}
+
+function TooltipContent({ payload }: any) {
+  const data = payload?.[0]?.payload || {}
+  return (
+    <TooltipContainer>
+      <p>
+        {data.label}: {data.tooltipValue}
+      </p>
+      <p>Quality: {data.quality}</p>
+    </TooltipContainer>
+  )
+}
--- src/pages/progress/components/progress-chart/progress-chart.styles.ts
+++ src/pages/progress/components/progress-chart/progress-chart.styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+import Card from '../../../../components/cards/card/card.component'
+import { getColorCarry } from '../../../../pipes/theme-color.pipe'
+
+export const Styles = styled(Card)`
+  margin-bottom: 2rem;
+
+  .chart {
+    &__title {
+      font-size: 1.125rem;
+      font-weight: 400;
+      color: ${getColorCarry('primaryDark_v2')};
+      align-items: center;
+      justify-content: space-between;
+
+      &-container {
+        margin-bottom: 2rem;
+      }
+    }
+
+    &__chart {
+      &-container {
+        width: auto;
+        margin-left: -1.75rem;
+      }
+    }
+  }
+`
