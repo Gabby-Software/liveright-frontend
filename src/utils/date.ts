894713ce9c3f6169590f5c5b2b262ec3b775d1b3
Andriy Halushka <andrushagalushka@gmail.com>
2021-09-28 17:48:15 +0300

    add date overlay utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -21,3 +21,38 @@ export function getDisabledMinutes(disableUntilNow: boolean, hour: number) {
 
   return []
 }
+
+export function hoursBetween(from: number, to: number): number[] {
+  const arr = []
+
+  for (let i = from; i <= to; i++) {
+    arr.push(i)
+  }
+
+  return arr
+}
+
+export function isOverlap(
+  start1?: string,
+  end1?: string,
+  start2?: string,
+  end2?: string
+): boolean {
+  try {
+    if (!(start1 && end1 && start2 && end2)) {
+      return false
+    }
+    // console.log(start1, end1, start2, end2)
+
+    const s1 = moment(start1, 'H:mm')
+    const e1 = moment(end1, 'H:mm')
+    const s2 = moment(start2, 'H:mm')
+    const e2 = moment(end2, 'H:mm')
+
+    // console.log(s1, e1)
+
+    return s1.isAfter(e2) && s2.isBefore(e1)
+  } catch (e) {
+    return false
+  }
+}
