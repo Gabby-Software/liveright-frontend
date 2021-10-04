d57aac03b271d55a98a2c277ddb511ffef7249e0
Ghulam213 <gmuhammad213@gmail.com>
2021-10-04 18:21:32 +0500

    settings page UI minus billing methods

---
+++ src/pages/settings/settings-desktop/settings-desktop.component.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useParams } from 'react-router'
+
+import Tabs from '../../../components/tabs/tabs.component'
+import { useTranslation } from '../../../modules/i18n/i18n.hook'
+import { content } from '../settings.context'
+import { ParamsType } from '../settings.types'
+import Styles from './settings-desktop.style'
+
+const DesktopSettings = () => {
+  const params = useParams<ParamsType>()
+  const { t } = useTranslation()
+  const [activeTab, setActiveTab] = useState<string>(
+    params.tab || 'notifications'
+  )
+
+  return (
+    <Styles>
+      <Tabs
+        className="settings__tabs"
+        activeKey={activeTab}
+        onChange={(activeKey) => setActiveTab(activeKey)}
+        tabs={content.map((c) => ({
+          key: c.key,
+          label: c.label,
+          renderContent: () => <c.component />
+        }))}
+      />
+    </Styles>
+  )
+}
+
+export default DesktopSettings
