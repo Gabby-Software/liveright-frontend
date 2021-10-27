249fce4334cef3d11434dd1c10ca3c28bcbfbf0b
Andriy Halushka <andrushagalushka@gmail.com>
2021-10-27 20:18:49 +0300

    dashboard add calendar

--- src/pages/dashboard/components/dashboard-calendar/dashboard-calendar.component.tsx
+++ src/pages/dashboard/components/dashboard-calendar/dashboard-calendar.component.tsx
@@ -1,5 +1,38 @@
+import { Link } from 'react-router-dom'
+
+import { CalendarIcon } from '../../../../assets/media/icons'
+import Button from '../../../../components/buttons/button/button.component'
+import Calendar from '../../../../components/calendar/calendar.component'
+import { Subtitle } from '../../../../components/typography'
+import { Routes } from '../../../../enums/routes.enum'
 import { Styles } from './dashboard-calendar.styles'
 
 export const DashboardCalendar = () => {
-  return <Styles></Styles>
+  return (
+    <Styles>
+      <div className="dashboard-calendar__title-container">
+        <Subtitle>Your Calender</Subtitle>
+
+        <Link to="/sessions">Schedule Sessions</Link>
+      </div>
+
+      <div className="dashboard-calendar__calendar-container">
+        <Calendar
+          view="day"
+          toolbar="secondary"
+          className="dashboard-calendar__calendar"
+        />
+      </div>
+
+      <Button
+        variant="secondary"
+        to={Routes.CALENDAR}
+        linkClassName="dashboard-calendar__btn-wrapper"
+        className="dashboard-calendar__btn"
+      >
+        <CalendarIcon />
+        Open Calender
+      </Button>
+    </Styles>
+  )
 }
