6083bd710c2e99498fe5fb3f4f7849b414d2bb93
Yurvel <opper@colorelephant.com>
2021-10-27 18:10:01 +0300

    added client`s  dashboard

--- src/pages/dashboard/dashboard.component.tsx
+++ src/pages/dashboard/dashboard.component.tsx
@@ -1,3 +1,4 @@
+import userTypes from '../../enums/user-types.enum'
 import { useAuth } from '../../hooks/auth.hook'
 import { useIsMobile } from '../../hooks/is-mobile.hook'
 import { useTitle } from '../../hooks/title.hook'
@@ -8,10 +9,11 @@ import { DashboardLatest } from './components/dashboard-latest/dashboard-latest.
 import { QuickLinks } from './components/dashboard-quick-links/dashboard-quick-links.component'
 import { DashboardRevenue } from './components/dashboard-revenue/dashboard-revenue.component'
 import Styles, { Container, ContainerGrid } from './dashboard.styles'
-
+const text =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 const Dashboard = () => {
   const isMobile = useIsMobile()
-  const { first_name, last_name } = useAuth()
+  const { first_name, last_name, type } = useAuth()
   useTitle(`Hello, ${first_name} ${last_name}`)
 
   const content = (
@@ -25,6 +27,9 @@ const Dashboard = () => {
       </ContainerGrid>
     </Styles>
   )
+  if (type === userTypes.CLIENT) {
+    return <div>{text}</div>
+  }
 
   return isMobile ? (
     <MobilePage
