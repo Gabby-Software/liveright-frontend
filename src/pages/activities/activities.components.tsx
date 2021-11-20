dd102c1eb64646f7f1d11e6014bb51c81a2b2a92
Andriy Halushka <andrushagalushka@gmail.com>
2021-11-20 11:35:16 +0200

    plans add button to training plans

--- src/pages/activities/activities.components.tsx
+++ src/pages/activities/activities.components.tsx
@@ -1,3 +1,10 @@
+import Button from '../../components/buttons/button/button.component'
+import { Routes } from '../../enums/routes.enum'
+
 export default function Activities() {
-  return <></>
+  return (
+    <>
+      <Button to={Routes.ACTIVITIES_TP}>Training Plans</Button>
+    </>
+  )
 }
