9a949bb57b6570d62e77596c656eb123dfca910d
Julius0505 <se7en.mini.123@protonmail.com>
2022-02-14 18:55:00 +0300

    no training plans in acitivities

--- src/pages/activities/training-plan/plans/plans.component.tsx
+++ src/pages/activities/training-plan/plans/plans.component.tsx
@@ -2,6 +2,7 @@ import moment from 'moment'
 import { useState } from 'react'
 import { Link, Redirect, useHistory, useParams } from 'react-router-dom'
 
+import { WorkoutIcon } from '../../../../assets/media/icons/activities'
 import Button from '../../../../components/buttons/button/button.component'
 import Card from '../../../../components/cards/card/card.component'
 import DataPagination from '../../../../components/data-pagination/data-pagination.component'
@@ -26,6 +27,7 @@ import { getActiveOrLatestRev } from '../../../../utils/api/activities'
 import { DATE_RENDER_FORMAT } from '../../../../utils/date'
 import { getRoute } from '../../../../utils/routes'
 import ActivitiesClient from '../../components/activities-client/activities-client.component'
+import EmptyPlan from '../../components/empty-plan/empty-plan.component'
 import PlanCard from '../../components/plan-card/plan-card.component'
 import { Styles } from '../../styles/plans-table.styles'
 import AddTrainingPlan from '../add-plan/add-plan.component'
@@ -128,184 +130,200 @@ export default function TrainingPlans() {
     return sortValue
   }
 
-  const content = (
-    <Styles>
-      {userType !== userTypes.CLIENT && (
-        <ActivitiesClient
-          viewActivity={false}
-          clientId={clientId}
-          onClientSwitch={(id) => {
-            history.push(getRoute(Routes.ACTIVITIES_TP, { clientId: id }))
-          }}
+  const content =
+    userType === userTypes.CLIENT && !trainingPlans.length ? (
+      <Styles>
+        <EmptyPlan
+          title="Training Plans"
+          text="There is no training plan yet..."
+          Icon={WorkoutIcon}
+          action={
+            <Button onClick={() => setAdd(true)}>Create Training Plan</Button>
+          }
         />
-      )}
-
-      <Card className="PlansTable__card">
-        {!isMobile && (
-          <>
-            <div className="PlansTable__title-container">
-              <Title>Training Plans</Title>
-
-              <div>
-                {clientId !== 'all' && (
-                  <Button onClick={() => setAdd(true)}>Create New Plan</Button>
-                )}
-                {clientId === 'all' && (
-                  <Tooltip title="Please select a client before creating a plan">
-                    <Button>Create New Plan</Button>
-                  </Tooltip>
-                )}
-              </div>
-            </div>
-          </>
+      </Styles>
+    ) : (
+      <Styles>
+        {userType !== userTypes.CLIENT && (
+          <ActivitiesClient
+            viewActivity={false}
+            clientId={clientId}
+            onClientSwitch={(id) => {
+              history.push(getRoute(Routes.ACTIVITIES_TP, { clientId: id }))
+            }}
+          />
         )}
 
-        {isLoading ? (
-          <LoadingPlaceholder spacing />
-        ) : (
-          <>
-            <div className="PlansTable__filters">
-              {userType !== userTypes.CLIENT && (
-                <ClientSelect
-                  value={clientId}
-                  id="training-plans-client"
-                  onChange={(e) =>
-                    history.push(
-                      getRoute(Routes.ACTIVITIES_TP, { clientId: e })
-                    )
-                  }
-                  placeholder="All Client"
+        <Card className="PlansTable__card">
+          {!isMobile && (
+            <>
+              <div className="PlansTable__title-container">
+                <Title>Training Plans</Title>
+
+                <div>
+                  {clientId !== 'all' && (
+                    <Button onClick={() => setAdd(true)}>
+                      Create New Plan
+                    </Button>
+                  )}
+                  {clientId === 'all' && (
+                    <Tooltip title="Please select a client before creating a plan">
+                      <Button>Create New Plan</Button>
+                    </Tooltip>
+                  )}
+                </div>
+              </div>
+            </>
+          )}
+
+          {isLoading ? (
+            <LoadingPlaceholder spacing />
+          ) : (
+            <>
+              <div className="PlansTable__filters">
+                {userType !== userTypes.CLIENT && (
+                  <ClientSelect
+                    value={clientId}
+                    id="training-plans-client"
+                    onChange={(e) =>
+                      history.push(
+                        getRoute(Routes.ACTIVITIES_TP, { clientId: e })
+                      )
+                    }
+                    placeholder="All Client"
+                    className="PlansTable__select"
+                  />
+                )}
+
+                <Select
+                  id="training-plans-statuses"
+                  options={[
+                    { label: 'Any Status', value: '' },
+                    { label: 'Active', value: 'active' },
+                    { label: 'Scheduled', value: 'scheduled' },
+                    { label: 'Inactive', value: 'inactive' }
+                  ]}
+                  value={status}
+                  onChange={(value) => setStatus(value)}
+                  placeholder="All Status"
                   className="PlansTable__select"
                 />
-              )}
+              </div>
 
-              <Select
-                id="training-plans-statuses"
-                options={[
-                  { label: 'Any Status', value: '' },
-                  { label: 'Active', value: 'active' },
-                  { label: 'Scheduled', value: 'scheduled' },
-                  { label: 'Inactive', value: 'inactive' }
-                ]}
-                value={status}
-                onChange={(value) => setStatus(value)}
-                placeholder="All Status"
-                className="PlansTable__select"
-              />
-            </div>
-
-            <div>
-              {isMobile ? (
-                <>
-                  {trainingPlans.map((row, index) => (
-                    <PlanCard
-                      key={index}
-                      plan={row}
-                      to={getRoute(Routes.ACTIVITIES_TP_ID, {
-                        clientId: row.account_id || clientId,
-                        id: row._id,
-                        revisionId: getActiveOrLatestRev(row)?._id
-                      })}
+              <div>
+                {isMobile ? (
+                  <>
+                    {trainingPlans.map((row, index) => (
+                      <PlanCard
+                        key={index}
+                        plan={row}
+                        to={getRoute(Routes.ACTIVITIES_TP_ID, {
+                          clientId: row.account_id || clientId,
+                          id: row._id,
+                          revisionId: getActiveOrLatestRev(row)?._id
+                        })}
+                      />
+                    ))}
+                  </>
+                ) : (
+                  <>
+                    <DataTable
+                      labels={LABELS}
+                      data={trainingPlans.sort(sortLogic)}
+                      keys={KEYS}
+                      sort={sorting}
+                      setSort={setSorting}
+                      round="10px"
+                      render={{
+                        name: (row) => (
+                          <Link
+                            to={getRoute(Routes.ACTIVITIES_TP_ID, {
+                              clientId: row.account_id || clientId,
+                              id: row._id,
+                              revisionId: getActiveOrLatestRev(row)?._id
+                            })}
+                            className="PlansTable__table-link"
+                          >
+                            <span>{row.name || '-'}</span>
+                          </Link>
+                        ),
+                        client: (row) => (
+                          <span>
+                            {row.account
+                              ? [
+                                  row.account?.user?.first_name,
+                                  row.account?.user?.last_name
+                                ]
+                                  .filter(Boolean)
+                                  .join(' ') || '-'
+                              : '-'}
+                          </span>
+                        ),
+                        days: (row) => (
+                          <span>{getActiveOrLatestRev(row)?.days_count}</span>
+                        ),
+                        start: (row) => (
+                          <span>
+                            {getActiveOrLatestRev(row)?.scheduled_start_on
+                              ? moment(
+                                  new Date(
+                                    getActiveOrLatestRev(
+                                      row
+                                    )?.scheduled_start_on
+                                  )
+                                ).format(DATE_RENDER_FORMAT)
+                              : '-'}
+                          </span>
+                        ),
+                        end: (row) => (
+                          <span>
+                            {getActiveOrLatestRev(row)?.scheduled_end_on
+                              ? moment(
+                                  new Date(
+                                    getActiveOrLatestRev(row)?.scheduled_end_on
+                                  )
+                                ).format(DATE_RENDER_FORMAT)
+                              : '-'}
+                          </span>
+                        ),
+                        status: (row) => (
+                          <StatusBadge
+                            status={getActiveOrLatestRev(
+                              row
+                            )?.status.toLowerCase()}
+                            className="PlansTable__table-status"
+                          >
+                            {capitalize(getActiveOrLatestRev(row)?.status)}
+                          </StatusBadge>
+                        )
+                      }}
                     />
-                  ))}
-                </>
-              ) : (
-                <>
-                  <DataTable
-                    labels={LABELS}
-                    data={trainingPlans.sort(sortLogic)}
-                    keys={KEYS}
-                    sort={sorting}
-                    setSort={setSorting}
-                    round="10px"
-                    render={{
-                      name: (row) => (
-                        <Link
-                          to={getRoute(Routes.ACTIVITIES_TP_ID, {
-                            clientId: row.account_id || clientId,
-                            id: row._id,
-                            revisionId: getActiveOrLatestRev(row)?._id
-                          })}
-                          className="PlansTable__table-link"
-                        >
-                          <span>{row.name || '-'}</span>
-                        </Link>
-                      ),
-                      client: (row) => (
-                        <span>
-                          {row.account
-                            ? [
-                                row.account?.user?.first_name,
-                                row.account?.user?.last_name
-                              ]
-                                .filter(Boolean)
-                                .join(' ') || '-'
-                            : '-'}
-                        </span>
-                      ),
-                      days: (row) => (
-                        <span>{getActiveOrLatestRev(row)?.days_count}</span>
-                      ),
-                      start: (row) => (
-                        <span>
-                          {getActiveOrLatestRev(row)?.scheduled_start_on
-                            ? moment(
-                                new Date(
-                                  getActiveOrLatestRev(row)?.scheduled_start_on
-                                )
-                              ).format(DATE_RENDER_FORMAT)
-                            : '-'}
-                        </span>
-                      ),
-                      end: (row) => (
-                        <span>
-                          {getActiveOrLatestRev(row)?.scheduled_end_on
-                            ? moment(
-                                new Date(
-                                  getActiveOrLatestRev(row)?.scheduled_end_on
-                                )
-                              ).format(DATE_RENDER_FORMAT)
-                            : '-'}
-                        </span>
-                      ),
-                      status: (row) => (
-                        <StatusBadge
-                          status={getActiveOrLatestRev(
-                            row
-                          )?.status.toLowerCase()}
-                          className="PlansTable__table-status"
-                        >
-                          {capitalize(getActiveOrLatestRev(row)?.status)}
-                        </StatusBadge>
-                      )
-                    }}
-                  />
-                  {!trainingPlans.length && <EmptyPlaceholder spacing />}
-                </>
-              )}
-            </div>
-          </>
-        )}
-        <div>
-          <DataPagination
-            page={meta.current_page}
-            total={meta.total}
-            setPage={onPage}
-          />
-        </div>
-        {/*<EmptyPlaceholder*/}
-        {/*  spacing*/}
-        {/*  icon*/}
-        {/*  text="There is no training plan yet... "*/}
-        {/*  action={*/}
-        {/*    isMobile ? (*/}
-        {/*      <Button onClick={() => setAdd(true)}>Create Training Plan</Button>*/}
-        {/*    ) : undefined*/}
-        {/*  }*/}
-        {/*/>*/}
-      </Card>
-    </Styles>
-  )
+                    {!trainingPlans.length && <EmptyPlaceholder spacing />}
+                  </>
+                )}
+              </div>
+            </>
+          )}
+          <div>
+            <DataPagination
+              page={meta.current_page}
+              total={meta.total}
+              setPage={onPage}
+            />
+          </div>
+          {/*<EmptyPlaceholder*/}
+          {/*  spacing*/}
+          {/*  icon*/}
+          {/*  text="There is no training plan yet... "*/}
+          {/*  action={*/}
+          {/*    isMobile ? (*/}
+          {/*      <Button onClick={() => setAdd(true)}>Create Training Plan</Button>*/}
+          {/*    ) : undefined*/}
+          {/*  }*/}
+          {/*/>*/}
+        </Card>
+      </Styles>
+    )
 
   return isMobile ? (
     <MobilePage
