deea31f09eef4c73147eecdd1bdaef011533a719
Julius0505 <se7en.mini.123@protonmail.com>
2022-02-17 01:45:40 +0300

    main views for Day Targets days

---
+++ src/pages/activities/components/day-diet-target-card/day-diet-target-card.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Styles = styled.div`
+  .DayDietTargetCard {
+    &__item {
+      margin-bottom 1rem;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+`
